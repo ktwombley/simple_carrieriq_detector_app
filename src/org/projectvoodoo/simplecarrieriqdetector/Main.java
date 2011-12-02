b7e00b729266b25f08f4b4a34233b79f23cb7aae
ktwombley <ktwombley@gmail.com>
2011-12-02 10:41:13 -0600

    Refactored into an asynctask

--- src/org/projectvoodoo/simplecarrieriqdetector/Main.java
+++ src/org/projectvoodoo/simplecarrieriqdetector/Main.java
@@ -2,55 +2,101 @@
 package org.projectvoodoo.simplecarrieriqdetector;
 
 import org.projectvoodoo.simplecarrieriqdetector.Detect.DetectTest;
-
 import android.app.Activity;
 import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+
 
 public class Main extends Activity {
 
+	private DetectorTask dt = new DetectorTask();
+
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.main);
 
-        Detect detect = new Detect();
-        detect.findEverything();
-        detect.dumpFoundInLogcat();
+        dt = new DetectorTask();
+        dt.execute();  
+        
 
-        int detectionScore = detect.getDetectionScore();
+    }
 
-        TextView score = (TextView) findViewById(R.id.score);
-        score.setText(detectionScore + "");
+    private class DetectorTask extends AsyncTask<Void, Integer, Integer> {
+    	
+    	private Detect detect;
 
-        LinearLayout details = (LinearLayout) findViewById(R.id.details_list);
+    	@Override
+		protected void onPostExecute(Integer result) {
+			// TODO Auto-generated method stub
+			super.onPostExecute(result);
+			
 
-        for (DetectTest test : detect.getFound().keySet()) {
+			TextView score = (TextView) findViewById(R.id.score);
+			score.setText(result + "");
 
-            TextView title = new TextView(this);
-            title.setText(test.name + "\nconfidence level: " + test.confidenceLevel);
-            title.setPadding(8, 8, 8, 8);
-            title.setTextSize(20);
-            title.setTextColor(Color.WHITE);
-            details.addView(title);
+			LinearLayout details = (LinearLayout) findViewById(R.id.details_list);
 
-            if (detect.getFound().get(test).size() > 0) {
-                for (String line : detect.getFound().get(test)) {
-                    TextView content = new TextView(this);
-                    content.setText(line);
-                    content.setPadding(8, 0, 8, 0);
-                    details.addView(content);
-                }
-            } else {
-                TextView content = new TextView(this);
-                content.setText("Nothing found");
-                content.setPadding(8, 0, 8, 0);
-                details.addView(content);
-            }
-        }
+			for (DetectTest test : detect.getFound().keySet()) {
+
+				TextView title = new TextView(Main.this);
+				title.setText(test.name + "\nconfidence level: " + test.confidenceLevel);
+				title.setPadding(8, 8, 8, 8);
+				title.setTextSize(20);
+				title.setTextColor(Color.WHITE);
+				details.addView(title);
+
+				if (detect.getFound().get(test).size() > 0) {
+					for (String line : detect.getFound().get(test)) {
+						TextView content = new TextView(Main.this);
+						content.setText(line);
+						content.setPadding(8, 0, 8, 0);
+						details.addView(content);
+					}
+				} else {
+					TextView content = new TextView(Main.this);
+					content.setText("Nothing found");
+					content.setPadding(8, 0, 8, 0);
+					details.addView(content);
+				}
+			}
+		}
+		@Override
+		protected void onProgressUpdate(Integer... values) {
+			// TODO Auto-generated method stub
+			super.onProgressUpdate(values);
+		}
+		@Override
+		protected void onPreExecute() {
+			super.onPreExecute();
+			
+			ProgressBar pb = (ProgressBar) findViewById(R.id.detectionProgress);
+			pb.setVisibility(View.VISIBLE);
+		}
+		@Override
+		protected void onCancelled() {
+			super.onCancelled();
+			
+			ProgressBar pb = (ProgressBar) findViewById(R.id.detectionProgress);
+			pb.setProgress(0);
+			pb.setVisibility(View.INVISIBLE);
+		}
+
+		protected Integer doInBackground(Void... params) {
+			detect = new Detect();
+			detect.findEverything();
+			detect.dumpFoundInLogcat();
+
+			return detect.getDetectionScore();
+
+		}
 
     }
 }
